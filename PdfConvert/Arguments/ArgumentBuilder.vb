060aa49253766700c7524df70479b9fde5005c86
xieguigang <xie.guigang@gcmodeller.org>
2018-10-24 11:14:55 +0800

    how to build cli arguments?

---
+++ PdfConvert/Arguments/ArgumentBuilder.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Runtime.CompilerServices
+
+Namespace Arguments
+
+    Public Module BuilderExtensions
+
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="document"></param>
+        ''' <returns></returns>
+        <Extension> Public Function GetArgumentString(document As PDFContent) As String
+
+        End Function
+    End Module
+End Namespace
