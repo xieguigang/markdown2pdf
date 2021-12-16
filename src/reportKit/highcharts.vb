b4f4972d4091fcac10842f5d5fa9a130d13f5000
guigang xie <gg.xie@bionovogene.com>
2021-12-16 16:24:18 +0800

    bar chart

--- src/reportKit/highcharts.vb
+++ src/reportKit/highcharts.vb
@@ -1,10 +1,14 @@
 ﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Internal.Object
 Imports SMRUCC.Rsharp.Runtime.Interop
 Imports SMRUCC.WebCloud.JavaScript.highcharts
+Imports SMRUCC.WebCloud.JavaScript.highcharts.BarChart
 Imports SMRUCC.WebCloud.JavaScript.highcharts.Javascript
 Imports SMRUCC.WebCloud.JavaScript.highcharts.PieChart
+Imports SMRUCC.WebCloud.JavaScript.highcharts.viz3D
 Imports any = Microsoft.VisualBasic.Scripting
 Imports chartProfiles = SMRUCC.WebCloud.JavaScript.highcharts.chart
 
@@ -19,6 +23,8 @@ Module highcharts
 
         If TypeOf chart Is PieChart Then
             Return any.ToString(divId).WriteJavascript(DirectCast(chart, PieChart))
+        ElseIf TypeOf chart Is BarChart Then
+            Return any.ToString(divId).WriteJavascript(DirectCast(chart, BarChart))
         Else
             Throw New NotImplementedException
         End If
@@ -61,4 +67,67 @@ Module highcharts
         Return chart
     End Function
 
+    <ExportAPI("barchart")>
+    Public Function BarChart(data As list,
+                             Optional title As String = "title",
+                             Optional subtitle As String = "subtitle",
+                             Optional ylab As String = "Y",
+                             Optional serialName As String = "data",
+                             Optional env As Environment = Nothing) As BarChart
+
+        Dim values As Dictionary(Of String, Double) = data.AsGeneric(Of Double)(env)
+        Dim x As New Axis With {
+            .categories = values.Keys.ToArray,
+            .labels = New labelOptions With {
+                .skew3d = True,
+                .style = New styleOptions With {
+                    .fontSize = "16px"
+                }
+            }
+        }
+        Dim chart As New BarChart With {
+            .chart = New chart With {
+                .type = "column",
+                .options3d = New options3d With {
+                    .enabled = True,
+                    .alpha = 15,
+                    .beta = 15,
+                    .viewDistance = 25,
+                    .depth = 40
+                }
+            },
+            .title = New title With {.text = title},
+            .xAxis = x,
+            .yAxis = New Axis With {
+                .min = 0,
+                .title = New title With {
+                    .text = ylab,
+                    .skew3d = True
+                },
+                .allowDecimals = False
+            },
+            .legend = New legendOptions With {
+                .reversed = False
+            },
+            .tooltip = New tooltip With {
+                .headerFormat = "<b>{point.key}</b><br>" ', .pointFormat = "<span style=""color:{series.color}"">{series.name}: {point.y:.2f}</span>"
+            },
+            .plotOptions = New plotOptions With {
+                .column = New columnOptions With {
+                    .stacking = "normal",
+                    .depth = 40
+                }
+            },
+            .series = {
+                New GenericDataSerial With {
+                    .name = serialName,
+                    .data = x.categories _
+                        .Select(Function(d) values(d)) _
+                        .ToArray
+                }
+            }
+        }
+
+        Return chart
+    End Function
 End Module
