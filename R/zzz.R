8c4fa88fea8a23bcaa0ec8d51f8d2b10b08777ae
xieguigang <xie.guigang@gcmodeller.org>
2021-12-18 20:20:30 +0800

    configs

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,8 @@
 imports ["html", "pdf"] from "reportKit";
 
+#' Config for the runtime environment
+#' at package startup.
+#' 
 const zzz as function() {
-	
+	options(wkhtmltopdf = "/usr/local/bin/wkhtmltopdf");
 }
