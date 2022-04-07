6a4bac12ee1cf9737ca7f17583a0cc750bd8a3be
guigang xie <gg.xie@bionovogene.com>
2022-04-07 15:16:00 +0800

    fix of the temp dir path

--- src/reportKit/htmlReport.vb
+++ src/reportKit/htmlReport.vb
@@ -205,7 +205,7 @@ getStringValue:
         End If
 
         If copyToTemp Then
-            Dim tmpdir As String = TempFileSystem.GetAppSysTempFile(ext:="__pdf_template", sessionID:=App.PID.ToHexString, prefix:="wkhtmltopdf")
+            Dim tmpdir As String = TempFileSystem.GetAppSysTempFile(ext:="__pdf_template", sessionID:="", prefix:="wkhtmltopdf")
             Call New Directory(template).CopyTo(tmpdir).ToArray
             template = tmpdir
         End If
