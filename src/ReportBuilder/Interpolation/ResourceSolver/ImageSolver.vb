68da90abb6ab5cce6d440352f2e2829154c2c717
guigang xie <gg.xie@bionovogene.com>
2022-04-07 10:01:16 +0800

    split source file modules

---
+++ src/ReportBuilder/Interpolation/ResourceSolver/ImageSolver.vb
@@ -0,0 +1,36 @@
+﻿Imports System.Net
+Imports System.Text
+Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports Microsoft.VisualBasic.Data.csv.IO
+Imports Microsoft.VisualBasic.MIME.Html.Language.CSS
+Imports any = Microsoft.VisualBasic.Scripting
+
+Public Class ImageSolver : Inherits ResourceSolver
+
+    Public Sub New(res As ResourceDescription)
+        MyBase.New(res)
+    End Sub
+
+    Public Overrides Function GetHtml(workdir As String) As String
+        Dim filepath As String = getfile(workdir)
+        Dim html As Boolean = resource.options.TryGetValue("html", [default]:=False)
+
+        If html Then
+            If filepath.FileExists Then
+                Return $"<img style='{resource.styles}' src='{filepath}' />"
+            Else
+                Return any.ToString(resource.options.TryGetValue("missing", [default]:=""))
+            End If
+        Else
+            Return filepath
+        End If
+    End Function
+
+    Private Function getfile(workdir As String) As String
+        If resource.image.FileExists Then
+            Return resource.image
+        Else
+            Return $"{workdir}/{resource.image}"
+        End If
+    End Function
+End Class
---
+++ src/ReportBuilder/Interpolation/ResourceSolver/TextSolver.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Net
+Imports System.Text
+Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports Microsoft.VisualBasic.Data.csv.IO
+Imports Microsoft.VisualBasic.MIME.Html.Language.CSS
+Imports any = Microsoft.VisualBasic.Scripting
+
+Public Class TextSolver : Inherits ResourceSolver
+
+    Public Sub New(res As ResourceDescription)
+        MyBase.New(res)
+    End Sub
+
+    Public Overrides Function GetHtml(workdir As String) As String
+        If resource.styles.StringEmpty Then
+            Return WebUtility.HtmlEncode(resource.text)
+        Else
+            Return $"<span style='{resource.styles}'>{WebUtility.HtmlEncode(resource.text)}</span>"
+        End If
+    End Function
+End Class
