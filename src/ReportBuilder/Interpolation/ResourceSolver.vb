3fb9ae44000bde1e2c2a47e908a06266a749e882
guigang xie <gg.xie@bionovogene.com>
2022-04-07 09:57:46 +0800

    not working?

--- src/ReportBuilder/Interpolation/ResourceSolver.vb
+++ src/ReportBuilder/Interpolation/ResourceSolver.vb
@@ -3,6 +3,7 @@ Imports System.Text
 Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.Data.csv.IO
 Imports Microsoft.VisualBasic.MIME.Html.Language.CSS
+Imports any = Microsoft.VisualBasic.Scripting
 
 Public MustInherit Class ResourceSolver
 
@@ -27,6 +28,21 @@ Public Class ImageSolver : Inherits ResourceSolver
     End Sub
 
     Public Overrides Function GetHtml(workdir As String) As String
+        Dim filepath As String = getfile(workdir)
+        Dim html As Boolean = resource.options.TryGetValue("html", [default]:=False)
+
+        If html Then
+            If filepath.FileExists Then
+                Return $"<img style='{resource.styles}' src='{filepath}' />"
+            Else
+                Return any.ToString(resource.options.TryGetValue("missing", [default]:=""))
+            End If
+        Else
+            Return filepath
+        End If
+    End Function
+
+    Private Function getfile(workdir As String) As String
         If resource.image.FileExists Then
             Return resource.image
         Else
