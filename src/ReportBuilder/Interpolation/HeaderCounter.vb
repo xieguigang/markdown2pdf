5650b3cd8f9a16f861471d4db13f13f4cd15a76d
xieguigang <xie.guigang@gcmodeller.org>
2022-06-04 01:16:04 +0800

    Create HeaderCounter.vb

---
+++ src/ReportBuilder/Interpolation/HeaderCounter.vb
@@ -0,0 +1,11 @@
+﻿Public Class HeaderCounter
+
+    Public Property h1 As Integer = 1
+    Public Property h2 As Integer = 1
+    Public Property h3 As Integer = 1
+    Public Property h4 As Integer = 1
+
+    Public Overrides Function ToString() As String
+        Return $"{h1}.{h2}.{h3}.{h4}"
+    End Function
+End Class
