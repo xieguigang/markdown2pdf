c8e3f522839d805ed3ddfa2d286cc2266a67561c
xieguigang <xie.guigang@gcmodeller.org>
2022-08-13 00:41:42 +0800

    Update sync_gitlink.cmd

--- .github/sync_gitlink.cmd
+++ .github/sync_gitlink.cmd
@@ -2,12 +2,7 @@
 
 REM git remote add gitlink https://gitlink.org.cn/xieguigang/markdown2pdf.git
 
-git pull local HEAD
-git pull gitee HEAD
 git pull gitlink HEAD
-
-git push local HEAD
-git push gitee HEAD
 git push gitlink HEAD
 
 echo synchronization of this code repository job done!
