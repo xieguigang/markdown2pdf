be6296cb010d11a733529f9b08c8a17cf622bf9b
xieguigang <xie.guigang@gcmodeller.org>
2023-05-08 11:47:35 +0800

    Create index.d.ts

---
+++ @export/index.d.ts
@@ -0,0 +1,14 @@
+﻿// export R# source type define for javascript/typescript language
+//
+// package_source=Markdown2PDF
+
+declare namespace Markdown2PDF {
+   /**
+   */
+   function check_wkhtmltopdf(): object;
+   module  {
+      /**
+      */
+      function onLoad(): object;
+   }
+}
