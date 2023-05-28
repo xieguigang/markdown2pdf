7dd869bf53bf2a11bdf2c8f688f292f6360705ef
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-28 16:59:11 +0800

    update ts/js type export header files

--- @export/htmlReport.d.ts
+++ @export/htmlReport.d.ts
@@ -1,5 +1,7 @@
 ﻿// export R# package module type define for javascript/typescript language
 //
+//    imports "htmlReport" from "reportKit";
+//
 // ref=reportKit.htmlReportEngine@reportKit, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
 
 /**
