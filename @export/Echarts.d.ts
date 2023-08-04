41ee61113523f4673cf0f2ec5e6043b6637c5cca
guigang xie <xie.guigang@gcmodeller.org>
2023-08-04 10:37:36 +0800

    run package module msbuild

--- @export/Echarts.d.ts
+++ @export/Echarts.d.ts
@@ -5,9 +5,15 @@
 // ref=reportKit.Echarts@reportKit, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
 
 /**
+ * Echarts plot data helper, generate echart plot data object from .net clr object
+ * 
 */
 declare namespace Echarts {
    /**
+    * Convert the sciBASIC.NET network graph object as the e-charts graph object
+    * 
+    * 
+     * @param g -
    */
    function json_graph(g: object): object;
 }
--- man/reportKit/Echarts/Echarts.md
+++ man/reportKit/Echarts/Echarts.md
@@ -1,5 +1,5 @@
 # Echarts
 
+Echarts plot data helper, generate echart plot data object from .net clr object
 
-
-+ [json_graph](Echarts/json_graph.1) 
++ [json_graph](Echarts/json_graph.1) Convert the sciBASIC.NET network graph object as the e-charts graph object
