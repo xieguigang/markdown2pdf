c2895f90114d4a9f55e69c3b2b35b284d0117848
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-04 11:08:01 +0800

    rendering html paragraph

--- src/LaTex/LaTex/TexRender.vb
+++ src/LaTex/LaTex/TexRender.vb
@@ -2,5 +2,7 @@ Imports Microsoft.VisualBasic.MIME.text.markdown
 
 Public Class TexRender : Inherits Render
 
+    Public Overrides Function Paragraph(text As String, CreateParagraphs As Boolean) As String
 
+    End Function
 End Class
