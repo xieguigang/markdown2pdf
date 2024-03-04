5945613d51895539a5d66928aa08be4c81a870da
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-04 10:34:11 +0800

    configs run test

--- src/LaTex/LaTex/test/Program.vb
+++ src/LaTex/LaTex/test/Program.vb
@@ -1,7 +1,17 @@
-Imports System
+Imports Microsoft.VisualBasic.MIME.text.markdown
 
 Module Program
     Sub Main(args As String())
+        convertTest()
         Console.WriteLine("Hello World!")
+    End Sub
+
+    Sub convertTest()
+        Dim md As String = "E:\GCModeller\src\workbench\markdown2pdf\src\LaTex\test\test1.md".ReadAllText
+        Dim tex As String = New MarkdownHTML().Transform(md)
+
+        Call Console.WriteLine(tex)
+
+        Pause()
     End Sub
 End Module
