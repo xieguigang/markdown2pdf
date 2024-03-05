a39d466a48a1ee88b1c015c53d166b8d31cbc845
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-05 13:27:00 +0800

    fix of the paragraph syntax in latax

--- src/LaTex/LaTex/TexRender.vb
+++ src/LaTex/LaTex/TexRender.vb
@@ -10,13 +10,24 @@ Public Class TexRender : Inherits Render
 
     Public Overrides Function Paragraph(text As String, CreateParagraphs As Boolean) As String
         ' replace the leading whitespace as the tex tag 
-        Dim p As String = _leadingWhitespace.Replace(text, If(CreateParagraphs, "\begin{document}" & vbCrLf & vbCrLf, ""))
-        Dim html As String = p & (If(CreateParagraphs, vbCrLf & vbCrLf & "\end{document}", ""))
+        Dim p As String = _leadingWhitespace.Replace(text, If(CreateParagraphs, "\paragraph{}" & vbCrLf & vbCrLf, ""))
+        Dim html As String = p & vbCrLf & vbCrLf
 
         Return html
     End Function
 
+    Public Overrides Function Header(text As String) As String
+        Throw New NotImplementedException()
+    End Function
+
     Public Overrides Function Document(text As String) As String
-        Return $"\documentclass[{Size},{BaseFontSizePt}pt]{{article}}" & vbCrLf & vbCrLf & text
+        Return $"\documentclass[{Size},{BaseFontSizePt}pt]{{article}}
+
+\begin{{document}}
+
+{text}
+
+\end{{document}}
+"
     End Function
 End Class
