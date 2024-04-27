a9f6f2898d0909aee602e7e94fa8ed22138d0b8e
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-27 22:20:04 +0800

    Update sync_multiplegit.cmd

--- .github/sync_multiplegit.cmd
+++ .github/sync_multiplegit.cmd
@@ -2,14 +2,14 @@
 
 REM git remote remove local 
 REM git remote add local http://192.168.0.232:8848/xieguigang/markdown2pdf.git
-REM git remote add local http://git.biodeep.cn/xieguigang/markdown2pdf.git
+REM git remote add biodeep http://git.biodeep.cn/xieguigang/markdown2pdf.git
 
 git pull gitlink HEAD
 git pull gitee HEAD
-git pull local HEAD
+git pull biodeep HEAD
 
 git push gitlink HEAD
 git push gitee HEAD
-git push local HEAD
+git push biodeep HEAD
 
 echo synchronization of this code repository job done!
