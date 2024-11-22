119dd5f519dde970c7c391542dc5611c2f5c7882
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-22 15:02:05 +0800

    removes un-used document files

--- .pkg/build.cmd
+++ .pkg/build.cmd
@@ -3,8 +3,9 @@
 SET drive=%~d0
 SET R_HOME=%drive%/GCModeller\src\R-sharp\App\net8.0
 SET pkg=./Markdown2PDF.zip
+SET js_url="https://rdocumentation.rsharp.net/assets/R_syntax.js"
 
-%R_HOME%/Rscript.exe --build /src ../ /save %pkg%
+%R_HOME%/Rscript.exe --build /src ../ /save %pkg%   --github-page %js_url%
 %R_HOME%/R#.exe --install.packages %pkg%
 
 pause
