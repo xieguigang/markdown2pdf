23e97c4f09fc8b9a0ccf255fb21d961ac340b786
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-23 11:00:07 +0800

    run package msbuild

--- @export/htmlReport.d.ts
+++ @export/htmlReport.d.ts
@@ -107,17 +107,19 @@ declare namespace htmlReport {
      *  the http url for http get and load the template document text.
    */
    function htmlTemplate(url: string): object;
-   /**
-    * do report data interpolation.
-    * 
-    * 
-     * @param template -
-     * @param metadata -
-     * @param env -
-     * 
-     * + default value Is ``null``.
-   */
-   function interpolate(template: object, metadata: object, env?: object): object;
+   module interpolate {
+      /**
+       * do report data interpolation.
+       * 
+       * 
+        * @param template -
+        * @param metadata -
+        * @param env -
+        * 
+        * + default value Is ``null``.
+      */
+      function html(template: object, metadata: object, env?: object): object;
+   }
    /**
     * Load resource files for build html report
     *  
